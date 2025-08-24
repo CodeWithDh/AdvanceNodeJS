c032842294e29bbbb7ac3001bdf947da9b520b20
Shivam <codewithdh@gmail.com>
2025-08-24 14:47:12 +0530

    SetUp express

--- LearnAuth/index.js
+++ LearnAuth/index.js
@@ -1,3 +1,13 @@
 import express from "express"
 const app=express()
-const PORT= process.env.PORT;
+const PORT= 8000;
+
+app.use(express.json())
+
+
+app.listen(PORT,()=>console.log(`Server is live at http://localhost:${PORT}`));
+
+app.get("/",(req,res)=>{
+    res.status(200).json({msg:"Server is live"})
+})
+
